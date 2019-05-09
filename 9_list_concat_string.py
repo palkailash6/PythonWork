a6f68e226a6370d3f2accf4293c037ba9105f9e2
palkailash6 <49854699+palkailash6@users.noreply.github.com>
2019-05-09 17:54:08 +0530

    Delete 9_list_concat_string.py

--- 9_list_concat_string.py
+++
@@ -1,9 +0,0 @@
-class ListDemo:
-    def concatenate_list_data(values):
-        result = ''
-        for i in values:
-            result = result + str(i)
-        return result
-
-    print(concatenate_list_data([1, 15, 2, 5]))
-
